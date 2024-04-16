f98dfe51e6151ba21b5e386b6cd1eae7f32b387c
Madu-f0r0 <duherbert@gmail.com>
2024-04-16 18:51:26 +0000

    Created auth module

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = os.getenv('AUTH_TYPE', None)
+
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_req():
+    if auth is not None:
+        excluded_paths = [
+                '/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/'
+                ]
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Contains the definition of the class `Auth`
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Handles user basic authorization
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns true id a given path is not in the list of paths in
+        `excluded_paths
+        """
+        if path is not None and excluded_paths:
+            if path.endswith('/') is False:
+                path += '/'
+            if path in excluded_paths:
+                return False
+        return True
+
+
+    def authorization_header(self, request=None) -> str:
+        """Checks if a request contains an authorization header
+        and returns the specified authorization type if present
+        """
+        if request is not None:
+            return request.headers.get('Authorization')
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Pass
+        """
+        return None
