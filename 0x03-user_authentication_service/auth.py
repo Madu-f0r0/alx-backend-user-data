f57c19a98d3c9b93fb57dbb1cd61319b309b6b1f
Madu-f0r0 <duherbert@gmail.com>
2024-04-25 11:24:00 +0000

    Initial set up for custom user authentication service

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """Returns the salted hash of a password
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
